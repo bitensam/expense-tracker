1912e7148fea537b49bab298f91eb2f3a501fe75
Jakub Wojcik <j.wojcik1994@gmail.com>
2021-09-14 15:24:27 +0200

    Add Card component

--- expense-tracker/src/components/ExpenseItemsContainer.js
+++ expense-tracker/src/components/ExpenseItemsContainer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 //styles
 import './ExpenseItemsContainer.css';
 
 const ExpenseItemsContainer = (props) => {
   return (
-    <div className='expenses'>
+    <Card className='expenses'>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -26,7 +27,7 @@ const ExpenseItemsContainer = (props) => {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
   );
 };
 
