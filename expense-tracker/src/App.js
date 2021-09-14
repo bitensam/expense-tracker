4c2332a7f107684c3378410b7b24561c09ed623c
Jakub Wojcik <j.wojcik1994@gmail.com>
2021-09-14 15:40:49 +0200

    Add new structure of directories folders

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItemsContainer from './components/ExpenseItemsContainer';
+import ExpenseItemsContainer from './components/Expenses/ExpenseItemsContainer';
 
 function App() {
   const expenses = [
@@ -30,7 +30,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItemsContainer items={expenses} />
+      <ExpenseItemsContainer items={expenses} classes />
     </div>
   );
 }
