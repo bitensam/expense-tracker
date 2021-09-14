0d8d1901c9484b02906d57eceb8b5a585acaaeda
Jakub Wojcik <j.wojcik1994@gmail.com>
2021-09-14 12:28:02 +0200

    Add first styles to ExpenseItem comp

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import './ExpenseItem.css';
 
 const ExpenseItem = () => {
-  return <h2>Expense Item</h2>;
+  return (
+    <div className='expense-item'>
+      <div>28th September 2021</div>
+      <div className='expense-item__description'>
+        <h2>Car Insurance</h2>
+        <div className='expense-item__price'>$234.78</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
