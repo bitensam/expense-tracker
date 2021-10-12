52fe87e0dd2ace090d112e5cfbc57299b0ed4a81
Jakub Wojcik <j.wojcik1994@gmail.com>
2021-10-12 15:05:07 +0200

    Add conditional content - showing NewExpenses after click on the button Add New Expense

--- expense-tracker/src/components/NewExpenses/NewExpense.js
+++ expense-tracker/src/components/NewExpenses/NewExpense.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 //components
 import NewExpenseForm from './NewExpenseForm';
 //styles
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   // lifting state up from NewExpenseForm
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseFormData = {
@@ -12,11 +22,20 @@ const NewExpense = (props) => {
       id: Math.random().toString(),
     };
     props.onAddNewExpense(expenseFormData);
+    setIsEditing(false);
   };
 
   return (
     <div className='new-expense'>
-      <NewExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add new expense</button>
+      )}
+      {isEditing && (
+        <NewExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
